46032a597b8c292205fbd77e46f3451f4735425e
gusplayer <fosterstudios.colombia@gmail.com>
2017-07-24 19:14:16 -0500

    La mayoria de rutas funcionando

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Controller\Auth;
 
 use App\User;
+use App\Modulo;
+use App\Pregunta;
 use DB;
+
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -52,14 +56,100 @@ class HomeController extends Controller
               ]);
     }
 
-    public function entrarTest()
+    public function regionActual()
     {
-        return view('contacto');
+        $modulo = Modulo::where('estado',1)->first();
+
+        return response()->json([
+            'region' => 1,
+            'descripcion' => $modulo->descripcion
+        ]);
     }
 
-    public function guardarPregunta()
+    public function entrarTest(Request $request)
     {
-        return view('restringido');
+        $cedula = $request->input('cedula');
+
+        $usuario = User::find($cedula);
+
+        //test al que tiene acceso el usuario
+        $test=$usuario->ronda;
+        $pregunta=$usuario->pregunta;
+
+        //Verificar Modulo/ No. test
+        $modulo = Modulo::where('estado',1)->first();
+
+        //Si el usuario esta en el test correcto
+        if(($modulo->id) == $test)
+        {
+          return response()->json([
+              'autorizado' => 'true',
+              'pregunta' => $usuario->pregunta,
+              'descripcion' => "continua respondiendo el test desde la pregunta ".$usuario->pregunta
+          ]);
+        }
+        //si el usuario no tiene permitido
+        else
+        {
+          return response()->json([
+              'autorizado' => 'false',
+              'pregunta' => 'null',
+              'descripcion' => "el usuario ya presento el test o no esta autorizado"
+          ]);
+        }
+    }
+
+    public function guardarPregunta(Request $request)
+    {
+        $cedula = $request->input('cedula');
+        $test = $request->input('test');
+        $pregunta = $request->input('pregunta');
+        $puntaje = $request->input('puntaje');
+        $fecha = Carbon::now();
+
+        $modulo = Modulo::where('estado',1)->first();
+        $usuario = User::find($cedula);
+        //Si el usuario esta en el test correcto
+        if(($modulo->id) == $test)
+        {
+          //Test correcto, vigente, guardamos la respuesta
+          if(($usuario->pregunta)<=10){
+
+            $pregunta2= new Pregunta;
+            $pregunta2->cedula=$cedula;
+            $pregunta2->test=$test;
+            $pregunta2->pregunta=$pregunta;
+            $pregunta2->puntaje=$puntaje;
+            $pregunta2->fecha=$fecha;
+            $pregunta2->save();
+
+            $nopregunta=1+$usuario->pregunta;
+            $usuario->pregunta= $nopregunta;
+            $usuario->save();
+
+            return response()->json([
+                'guardado' => 'true',
+                'pregunta_guardada' => $nopregunta-1,
+                'descripcion' => "continua a la pregunta No. ".$nopregunta
+            ]);
+          }
+          else {
+            return response()->json([
+                'guardado' => 'false',
+                'pregunta_siguiente' => 'null',
+                'descripcion' => "El usuario ya presento todas las preguntas, ir a resultado"
+            ]);
+          }
+
+        }
+        else
+        {
+          //test no correcto, no se guardara.
+          return response()->json([
+              'guardado' => 'false',
+              'descripcion' => "el usuario ya presento el test o no esta autorizado"
+          ]);
+        }
     }
 
     public function resultadoTest()
@@ -67,10 +157,7 @@ class HomeController extends Controller
         return view('suspendido');
     }
 
-    public function regionActual()
-    {
-        return view('error');
-    }
+
 
     public function mostrarRanking()
     {
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,38 +4,40 @@
 //Autenticacion con cedula
 Route::post('api/cedula', array('uses' => 'HomeController@authCedula'));
 
-
 //bienvenida, solo la primera vez que ingresa el usuario
 //Confirma que el usuario ya vio el tour, para que no se vuelva a mostrar
 Route::post('api/bienvenido', array('uses' => 'HomeController@confirmarBienvenida'));
 
+//Mostrar region actual
+//Solo una region esta activa a la vez
+Route::get('api/region_actual', array('uses' => 'HomeController@regionActual'));
 
 //El usuario ingresa al test
 //Validamos que no lo halla presentado
 //Validamos si ya lo ha empezado pero no lo ha terminado
-Route::get('api/entrar_test', array('uses' => 'HomeController@entrarTest'));
+Route::post('api/entrar_test', array('uses' => 'HomeController@entrarTest'));
+
 
 
 //guardar una pregunta del test
-Route::get('api/guardarPregunta', array('uses' => 'HomeController@guardarPregunta'));
+Route::post('api/guardar_pregunta', array('uses' => 'HomeController@guardarPregunta'));
 
 
 //guardar el resultado final del test
 Route::get('api/resultado_test', array('uses' => 'HomeController@resultadoTest'));
 
 
-//Mostrar region actual
-//Solo una region esta activa a la vez
-Route::get('api/region_actual', array('uses' => 'HomeController@regionActual'));
 
 
 //Mostrar ranking
 Route::get('api/ranking', array('uses' => 'HomeController@mostrarRanking'));
 
 
+
+
 //cuando no hay una ruta se envia a pantalla de no existe
 Route::get('{url}',function()
-    {    return 'ejemplo';    });
+    {    return redirect()->to('https://aventuraindependence.com/');   });
 
 Route::get('{url}/{alg}',function()
-    {    return 'ejemplo';    });
+    {    return redirect()->to('https://aventuraindependence.com/');   });
